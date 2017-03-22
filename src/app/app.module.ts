eb4ab9ff14db27b28b0cabe1fa584e092e02ba25
James Hester <james.hester@rapiot.com>
2017-03-22 00:41:09 -0700

    First route working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            }
+        ])
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
     ],
     bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+    
+}
