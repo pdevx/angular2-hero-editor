890784ed21bbd22d163ea8509866e25298f89d7e
James Hester <james.hester@rapiot.com>
2017-04-05 00:06:23 -0700

    Tabbed navigation menu with md-tab-nav-bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     styleUrls: ['./app/app.component.css'],
     template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+        <md-toolbar>{{title}}</md-toolbar>
+        <nav md-tab-nav-bar>
+            <a md-tab-link routerLink="/dashboard" routerLinkActive #rla1="routerLinkActive" [active]="rla1.isActive">Dashboard</a>
+            <a md-tab-link routerLink="/heroes"  routerLinkActive #rla2="routerLinkActive" [active]="rla2.isActive">Heroes</a>
         </nav>
         <router-outlet></router-outlet>
     `
